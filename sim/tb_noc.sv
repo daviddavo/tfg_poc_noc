db5e471e3ce17c30ec8fbb28f97d9c47b3e9b96a
David Davó <david@ddavo.me>
2022-02-06 12:45:40 +0100

    Randomized packet

--- sim/tb_noc.sv
+++ sim/tb_noc.sv
@@ -26,13 +26,41 @@ import noc_types::*;
 
 class Packet;
     flit_t flits[];
+    rand int dst_x;
+    rand int dst_y;
+    rand bit data[];
     
-    // data should be packed array of unknown dimension?
-    function new(int dst_x, int dst_y, bit data []); // data can be = empty
+    constraint dst {
+                   // Going to horizontal edge
+                   ((dst_x == 0 || dst_x == `MESH_HEIGHT) && dst_y inside {[1:`MESH_WIDTH-1]}) ||
+                   // or Going to vertical edge
+                   ((dst_y == 0 || dst_y == `MESH_WIDTH) && dst_x inside {[1:`MESH_HEIGHT-1]});
+                   };
+
+     // Maximum size: 10 flits
+     constraint data_size { data.size() inside {[1:10*`FLIT_DATA_WIDTH]}; };
+
+//    constraint dst_not_corners {
+//                                !(dst_x == 0 && dst_y == 0);
+//                                !(dst_x == 0 && dst_y == `MESH_WIDTH);
+//                                !(dst_x == `MESH_HEIGHT && dst_y == 0);
+//                                !(dst_x == `MESH_HEIGHT && dst_y == `MESH_WIDTH);
+//                                };
+                       
+    function new(int dst_x, int dst_y, bit data []);
+        this.dst_x = dst_x;
+        this.dst_y = dst_y;
+        this.data = data;
+        
+        this.genflits();
+    endfunction: new
+    
+    function void genflits();
         // Divide data into multiple flits
         // Make the last one a tail flit
-        int n_flits = data.size() / `FLIT_DATA_WIDTH;
-        int tail_length = data.size() % `FLIT_DATA_WIDTH;
+
+        int n_flits = this.data.size() / `FLIT_DATA_WIDTH;
+        int tail_length = this.data.size() % `FLIT_DATA_WIDTH;
         
         if (n_flits == 0) begin
             // Special case: no data. (TAIL still needed)
@@ -40,7 +68,7 @@ class Packet;
             this.flits[1] = '{TAIL, 0};
         end else begin
             // +1 because of the HEADER
-            this.flits = new[n_flits+1];
+            flits = new[n_flits+1];
 
             // Divide data into multiple flits
             for (int i = 1; i <= n_flits; i++) begin
@@ -49,13 +77,26 @@ class Packet;
                 // Little hack: If we access in a dyn array to an undefined index, it returns 0
                 // So we don't need to add the special case for tail flits
                 for (int j = 0; j < `FLIT_DATA_WIDTH; j++)
-                    this.flits[i].payload[j] = data[(i-1)*`FLIT_DATA_WIDTH + j];
+                    this.flits[i].payload[j] = this.data[(i-1)*`FLIT_DATA_WIDTH + j];
             end
         end
         
         // Put header in the front
-        this.flits[0] = Packet::build_header(dst_x, dst_y, tail_length);
-    endfunction: new
+        this.flits[0] = this.build_header(this.dst_x, this.dst_y, tail_length);
+    endfunction: genflits
+    
+    function void post_randomize();
+        // TODO: Randomize flit data
+    
+        this.genflits();
+    endfunction: post_randomize
+    
+    function void display();
+        $display("Packet to %0d,%0d with data size: %3d (%0d flits)", 
+            this.dst_x, this.dst_y, 
+            this.data.size(),
+            this.flits.size());
+    endfunction: display
     
     static function flit_t build_header(int dst_x, int dst_y, int tail_length = 0);
         static flit_hdr_t hdr;
@@ -106,7 +147,22 @@ module tb_noc;
 
         // Keep in the same line, but go EAST
         p = new(1, `MESH_WIDTH+1, data);
+        
         west_down[0].enable = 1;
+        foreach(p.flits[i]) begin
+            #10
+            west_down[0].flit = p.flits[i];
+        end
+        west_down[0].enable = 0;
+        
+        #30
+        for (int i = 0; i < 20; i++) begin
+            assert(p.randomize());
+            p.display();
+        end
+        
+        west_down[0].enable = 1;
+        $display("Sending flit to %0d, %0d", p.dst_x, p.dst_y);
         foreach(p.flits[i]) begin
             #10
             west_down[0].flit = p.flits[i];
